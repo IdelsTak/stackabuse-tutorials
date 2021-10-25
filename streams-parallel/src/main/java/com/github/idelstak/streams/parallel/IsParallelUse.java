637722bed7b434bfc8ba52aacd20dcd1e37e4adb
Hiram Kamau <hiram.kamau@outlook.com>
2021-10-25 14:10:15 +0300

    Create IsParallelUse.java

---
+++ streams-parallel/src/main/java/com/github/idelstak/streams/parallel/IsParallelUse.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2021
+ */
+package com.github.idelstak.streams.parallel;
+
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author Hiram K. <https://github.com/IdelsTak>
+ */
+public class IsParallelUse {
+    
+    public IsParallelUse() {
+    }
+    
+    public void printToConsole(Stream<String> logs){
+        logs.forEach(System.out::println);
+    }
+}
