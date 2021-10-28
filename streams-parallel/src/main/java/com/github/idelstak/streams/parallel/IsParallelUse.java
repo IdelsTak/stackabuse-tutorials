74c43fca6fc6b90997b06985e4b33845d94917c1
Hiram Kamau <hiram.kamau@outlook.com>
2021-10-28 15:30:03 +0300

    Update IsParallelUse.java

--- streams-parallel/src/main/java/com/github/idelstak/streams/parallel/IsParallelUse.java
+++ streams-parallel/src/main/java/com/github/idelstak/streams/parallel/IsParallelUse.java
@@ -10,11 +10,17 @@ import java.util.stream.Stream;
  * @author Hiram K. <https://github.com/IdelsTak>
  */
 public class IsParallelUse {
-    
+
     public IsParallelUse() {
     }
-    
-    public void printToConsole(Stream<String> logs){
-        logs.forEach(System.out::println);
+
+    public void printToConsole(Stream<String> logs) {
+//        logs.forEach(System.out::println);
+
+        if (logs.isParallel()) {
+            logs.forEachOrdered(System.out::println);
+        } else { //if logs is sequential
+            logs.forEach(System.out::println);
+        }
     }
 }
