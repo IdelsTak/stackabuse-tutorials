dc22209febaaf326d78b9c65f28a14a5dbb1c28f
Hiram Kamau <hiram.kamau@outlook.com>
2021-11-09 14:47:29 +0300

    Create ReverseTest.java

---
+++ collectingAndThen/src/test/java/com/github/idelstak/collectingandthen/ReverseTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2021
+ */
+package com.github.idelstak.collectingandthen;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ * @author Hiram K. <https://github.com/IdelsTak>
+ */
+public class ReverseTest {
+    
+    public ReverseTest() {
+    }
+
+    @Test
+    public void shouldReverseList() {
+        Stream<Integer> s = Stream.of(1,2,3);
+        Reverse r = new Reverse(s);
+        List<?> l = r.getList();
+        
+        assertEquals("[3, 2, 1]", l.toString());
+    }
+    
+}
