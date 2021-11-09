dd18f26eac45672b964db43b2f34fd8d467cf7a4
Hiram Kamau <hiram.kamau@outlook.com>
2021-11-09 14:47:13 +0300

    Create Reverse.java

---
+++ collectingAndThen/src/main/java/com/github/idelstak/collectingandthen/Reverse.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021
+ */
+package com.github.idelstak.collectingandthen;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.Collections.reverse;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+/**
+ *
+ * @author Hiram K. <https://github.com/IdelsTak>
+ */
+public class Reverse {
+
+    private final Stream<?> s;
+
+    public Reverse(Stream<?> s) {
+        this.s = s;
+    }
+
+    public List<?> getList() {
+        return s.collect(
+                collectingAndThen(
+                        toList(),
+                        l -> {
+                            reverse(l);
+                            return l;
+                        }
+                )
+        );
+    }
+}
