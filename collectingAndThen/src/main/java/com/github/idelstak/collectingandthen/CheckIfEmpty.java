efcd7537fb88eeef2928dfed468b4bc375c2af70
Hiram Kamau <hiram.kamau@outlook.com>
2021-11-11 20:56:12 +0300

    Create CheckIfEmpty.java

---
+++ collectingAndThen/src/main/java/com/github/idelstak/collectingandthen/CheckIfEmpty.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2021
+ */
+package com.github.idelstak.collectingandthen;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+/**
+ *
+ * @author Hiram K. <https://github.com/IdelsTak>
+ */
+public class CheckIfEmpty {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3);
+
+        Boolean empty = list.stream()
+                .collect(collectingAndThen(
+                        toList(),
+                        List::isEmpty
+                ));
+    }
+}
