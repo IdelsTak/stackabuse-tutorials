86fcdc49039f02ba70cc8ee848f41e5d0b1956b5
Hiram Kamau <hiram.kamau@outlook.com>
2021-11-11 20:56:33 +0300

    Create Singleton.java

---
+++ collectingAndThen/src/main/java/com/github/idelstak/collectingandthen/Singleton.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021
+ */
+package com.github.idelstak.collectingandthen;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+/**
+ *
+ * @author Hiram K. <https://github.com/IdelsTak>
+ */
+public class Singleton {
+
+    private final Stream<?> stream;
+
+    public Singleton(Stream<?> stream) {
+        this.stream = stream;
+    }
+
+    public Set<?> getSingleton() {
+        return stream.collect(collectingAndThen(
+                toList(),
+                l -> {
+                    if (l.size() > 1) {
+                        throw new IllegalArgumentException();
+                    } else {
+                        return l.isEmpty()
+                        ? Collections.emptySet()
+                        : Collections.singleton(l.get(0));
+                    }
+                }
+        ));
+    }
+
+}
