a7dd1221650853b6cba7c32fdefb7131508b1f64
Hiram Kamau <hiram.kamau@outlook.com>
2021-11-11 14:06:30 +0300

    Create BenchmarkRunner.java

---
+++ collectingAndThen/src/main/java/com/github/idelstak/collectingandthen/benchmark/BenchmarkRunner.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2021
+ */
+package com.github.idelstak.collectingandthen.benchmark;
+
+import java.io.IOException;
+import org.openjdk.jmh.Main;
+
+/**
+ *
+ * @author Hiram K. <https://github.com/IdelsTak>
+ */
+public class BenchmarkRunner {
+
+    public static void main(String[] args) throws IOException {
+        Main.main(args);
+    }
+}
