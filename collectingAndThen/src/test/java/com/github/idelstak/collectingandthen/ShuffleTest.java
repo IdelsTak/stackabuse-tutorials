216cb5140db9337b01c3336036b0ad426234f83b
Hiram Kamau <hiram.kamau@outlook.com>
2021-11-11 20:56:50 +0300

    Create ShuffleTest.java

---
+++ collectingAndThen/src/test/java/com/github/idelstak/collectingandthen/ShuffleTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2021
+ */
+package com.github.idelstak.collectingandthen;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ *
+ * @author Hiram K. <https://github.com/IdelsTak>
+ */
+public class ShuffleTest {
+    
+    public ShuffleTest() {
+    }
+
+    @Test
+    public void shouldShuffleList() {
+        Shuffle shuffle = new Shuffle(Stream.of(1,2,3));
+        List<?> list = shuffle.getList();
+        
+        assertNotEquals("[1, 2, 3]", list.toString());
+    }
+    
+}
