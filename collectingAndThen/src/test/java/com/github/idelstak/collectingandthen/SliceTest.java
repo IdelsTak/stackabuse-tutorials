dd8dc2e7cd3933868aa86ade6ef734a7dbf54b2f
Hiram Kamau <hiram.kamau@outlook.com>
2021-11-11 20:56:59 +0300

    Update SliceTest.java

--- collectingAndThen/src/test/java/com/github/idelstak/collectingandthen/SliceTest.java
+++ collectingAndThen/src/test/java/com/github/idelstak/collectingandthen/SliceTest.java
@@ -8,12 +8,14 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  *
  * @author Hiram K. <https://github.com/IdelsTak>
  */
 public class SliceTest {
-    
+
     public SliceTest() {
     }
 
@@ -22,8 +24,8 @@ public class SliceTest {
         Stream<Integer> s = IntStream.rangeClosed(0, 10).boxed();
         Slice slice = new Slice(s, 3, 7);
         List<Integer> l = slice.getList();
-        
-        System.out.println(l);
+
+        assertEquals("[3, 4, 5, 6, 7]", l.toString());
     }
-    
+
 }
