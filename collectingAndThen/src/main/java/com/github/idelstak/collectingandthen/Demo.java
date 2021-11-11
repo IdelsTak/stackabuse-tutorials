af4809b9171eaa1700a868b2f279ca53bd451f0c
Hiram Kamau <hiram.kamau@outlook.com>
2021-11-11 14:06:52 +0300

    Update Demo.java

--- collectingAndThen/src/main/java/com/github/idelstak/collectingandthen/Demo.java
+++ collectingAndThen/src/main/java/com/github/idelstak/collectingandthen/Demo.java
@@ -4,12 +4,14 @@
 package com.github.idelstak.collectingandthen;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collector;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.Collections.unmodifiableList;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toCollection;
 
 /**
  *
@@ -19,11 +21,11 @@ public class Demo {
 
     public static void main(String[] args) {
         Stream<Integer> stream = Stream.of(1, 2, 3);
-        
-        Collector<Integer, ?, List<Integer>> downstream = Collectors.toCollection(() -> new ArrayList<Integer>());
-        Function<List<Integer>, List<Integer>> finisher = list -> Collections.unmodifiableList(list);
-        Collector<Integer, ?, List<Integer>> collector = Collectors.collectingAndThen(downstream, finisher);
-        
-        List<Integer> list = stream.collect(collector);
+
+        Collector downstream = toCollection(ArrayList::new);
+        Function finisher = list -> unmodifiableList((List<Integer>) list);
+        Collector collector = collectingAndThen(downstream, finisher);
+
+        List<Integer> list = (List<Integer>) stream.collect(collector);
     }
 }
