7e64c8dda3277c37069a605c95b4597999772bfe
Hiram Kamau <hiram.kamau@outlook.com>
2021-11-11 20:56:29 +0300

    Create Shuffle.java

---
+++ collectingAndThen/src/main/java/com/github/idelstak/collectingandthen/Shuffle.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2021
+ */
+package com.github.idelstak.collectingandthen;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+/**
+ *
+ * @author Hiram K. <https://github.com/IdelsTak>
+ */
+public class Shuffle {
+
+    private final Stream<?> stream;
+    
+    public Shuffle(Stream<?> stream) {
+        this.stream = stream;
+    }
+    
+    public List<?> getList() {
+        return stream
+                .collect(collectingAndThen(
+                        toList(),
+                        l -> {
+                            Collections.shuffle(l);
+                            return l;
+                        }
+                ));
+    }
+}
