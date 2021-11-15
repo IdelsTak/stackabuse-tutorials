6f1544939ccafb6be0d445f5f27860e6e0718f57
Hiram Kamau <hiram.kamau@outlook.com>
2021-11-15 13:19:12 +0300

    Update Simple.java

--- collectingAndThen/src/main/java/com/github/idelstak/collectingandthen/Simple.java
+++ collectingAndThen/src/main/java/com/github/idelstak/collectingandthen/Simple.java
@@ -19,48 +19,48 @@ import static java.util.stream.Collectors.toList;
  */
 public class Simple {
 
-    public static void main(String[] args) {
-        List<Integer> list = Stream.of(1, 2, 3, 4, 5)
-                .collect(Collectors.collectingAndThen(
-                        Collectors.toList(),
-                        Collections::unmodifiableList) //finisher
-                );
-        System.out.println(list.getClass());
+  public static void main(String[] args) {
+    List<Integer> list = Stream.of(1, 2, 3, 4, 5)
+            .collect(Collectors.collectingAndThen(
+                    Collectors.toList(),
+                    Collections::unmodifiableList) //finisher
+            );
+    System.out.println(list.getClass());
 
-        List<String> list2 = Stream.of("a", "b", "c", "d")
-                .collect(Collectors.collectingAndThen(
-                        Collectors.toMap(Function.identity(), s -> s + s),
-                        map -> map.entrySet().stream()
-                ))
-                .map(Object::toString)
-                .collect(Collectors.collectingAndThen(
-                        Collectors.toList(),
-                        Collections::unmodifiableList));
-        list2.forEach(System.out::println);
+    List<String> list2 = Stream.of("a", "b", "c", "d")
+            .collect(Collectors.collectingAndThen(
+                    Collectors.toMap(Function.identity(), s -> s + s),
+                    map -> map.entrySet().stream()
+            ))
+            .map(Object::toString)
+            .collect(Collectors.collectingAndThen(
+                    Collectors.toList(),
+                    Collections::unmodifiableList));
+    list2.forEach(System.out::println);
 
-        List<Integer> list1 = Stream.of(12, 13, 14, 15)
-                .collect(
-                        //Supplier
-                        () -> new ArrayList<Integer>(),
-                        //Accumulator
-                        (l, e) -> l.add(e),
-                        //Combiner
-                        (l, ar) -> l.addAll(ar)
-                );
+    List<Integer> list1 = Stream.of(12, 13, 14, 15)
+            .collect(
+                    //Supplier
+                    () -> new ArrayList<Integer>(),
+                    //Accumulator
+                    (l, e) -> l.add(e),
+                    //Combiner
+                    (l, ar) -> l.addAll(ar)
+            );
 
-        List<Integer> collect = Stream.of(12, 13, 14, 15)
-                .collect(
-                        Collectors.collectingAndThen(
-                                toList(),
-                                Collections::unmodifiableList
-                        )
-                );
+    List<Integer> collect = Stream.of(12, 13, 14, 15)
+            .collect(
+                    Collectors.collectingAndThen(
+                            toList(),
+                            Collections::unmodifiableList
+                    )
+            );
 
-        System.out.println(list1.getClass() + " " + collect.getClass());
+    System.out.println(list1.getClass() + " " + collect.getClass());
 
-        List<Integer> unmodifiable = unmodifiableList(list1);
+    List<Integer> unmodifiable = unmodifiableList(list1);
 
-        System.out.println(unmodifiable.getClass().getSimpleName());
+    System.out.println(unmodifiable.getClass().getSimpleName());
 
-    }
+  }
 }
