472910eaf5cbad49389d7b8a3aed859b49aa519c
Hiram Kamau <hiram.kamau@outlook.com>
2021-11-15 13:18:28 +0300

    Update Person.java

--- collectingAndThen/src/main/java/com/github/idelstak/collectingandthen/benchmark/Person.java
+++ collectingAndThen/src/main/java/com/github/idelstak/collectingandthen/benchmark/Person.java
@@ -11,56 +11,56 @@ import java.util.Objects;
  */
 public class Person {
 
-    private final String firstName;
-    private final String lastName;
+  private final String firstName;
+  private final String lastName;
 
-    public Person(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+  public Person(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
-    public String getFirstName() {
-        return firstName;
-    }
+  public String getFirstName() {
+    return firstName;
+  }
 
-    public String getLastName() {
-        return lastName;
-    }
+  public String getLastName() {
+    return lastName;
+  }
 
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 53 * hash + Objects.hashCode(this.firstName);
-        hash = 53 * hash + Objects.hashCode(this.lastName);
-        return hash;
-    }
+  @Override
+  public int hashCode() {
+    int hash = 7;
+    hash = 53 * hash + Objects.hashCode(this.firstName);
+    hash = 53 * hash + Objects.hashCode(this.lastName);
+    return hash;
+  }
 
-    @Override
-    @SuppressWarnings("AccessingNonPublicFieldOfAnotherObject")
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Person other = (Person) obj;
-        if (!Objects.equals(this.firstName, other.firstName)) {
-            return false;
-        }
-        return Objects.equals(this.lastName, other.lastName);
+  @Override
+  @SuppressWarnings("AccessingNonPublicFieldOfAnotherObject")
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
     }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final Person other = (Person) obj;
+    if (!Objects.equals(this.firstName, other.firstName)) {
+      return false;
+    }
+    return Objects.equals(this.lastName, other.lastName);
+  }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("{firstName=").append(firstName);
-        sb.append(", lastName=").append(lastName);
-        sb.append('}');
-        return sb.toString();
-    }
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("{firstName=").append(firstName);
+    sb.append(", lastName=").append(lastName);
+    sb.append('}');
+    return sb.toString();
+  }
 
 }
