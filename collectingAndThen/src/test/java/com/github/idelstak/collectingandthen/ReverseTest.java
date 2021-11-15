e39b4ced899d8d2302ca295dacd3120a6589afd3
Hiram Kamau <hiram.kamau@outlook.com>
2021-11-15 13:19:32 +0300

    Update ReverseTest.java

--- collectingAndThen/src/test/java/com/github/idelstak/collectingandthen/ReverseTest.java
+++ collectingAndThen/src/test/java/com/github/idelstak/collectingandthen/ReverseTest.java
@@ -14,17 +14,17 @@ import static org.junit.Assert.assertEquals;
  * @author Hiram K. <https://github.com/IdelsTak>
  */
 public class ReverseTest {
-    
-    public ReverseTest() {
-    }
 
-    @Test
-    public void shouldReverseList() {
-        Stream<Integer> s = Stream.of(1,2,3);
-        Reverse r = new Reverse(s);
-        List<?> l = r.getList();
-        
-        assertEquals("[3, 2, 1]", l.toString());
-    }
-    
+  public ReverseTest() {
+  }
+
+  @Test
+  public void shouldReverseList() {
+    Stream<Integer> s = Stream.of(1, 2, 3);
+    Reverse r = new Reverse(s);
+    List<?> l = r.getList();
+
+    assertEquals("[3, 2, 1]", l.toString());
+  }
+
 }
