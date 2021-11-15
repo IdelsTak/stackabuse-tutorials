d25500282e503d29d85125e02e8012480e57bf02
Hiram Kamau <hiram.kamau@outlook.com>
2021-11-15 13:19:37 +0300

    Update SingletonTest.java

--- collectingAndThen/src/test/java/com/github/idelstak/collectingandthen/SingletonTest.java
+++ collectingAndThen/src/test/java/com/github/idelstak/collectingandthen/SingletonTest.java
@@ -15,23 +15,23 @@ import static org.junit.Assert.assertThrows;
  * @author Hiram K. <https://github.com/IdelsTak>
  */
 public class SingletonTest {
-    
-    public SingletonTest() {
-    }
 
-    @Test
-    @SuppressWarnings("ThrowableResultIgnored")
-    public void shouldCreateSingleton() {
-        Singleton s1 = new Singleton(Stream.of(1,2));
-        assertThrows(RuntimeException.class, () -> s1.getSingleton());
-        
-        Singleton s2 = new Singleton(Stream.empty());
-        Set<?> singleton = s2.getSingleton();
-        assertEquals("[]", singleton.toString());
-        
-        Singleton s3 = new Singleton(Stream.of(1));      
-        singleton = s3.getSingleton();
-        assertEquals("[1]", singleton.toString());
-    }
-    
+  public SingletonTest() {
+  }
+
+  @Test
+  @SuppressWarnings("ThrowableResultIgnored")
+  public void shouldCreateSingleton() {
+    Singleton s1 = new Singleton(Stream.of(1, 2));
+    assertThrows(RuntimeException.class, () -> s1.getSingleton());
+
+    Singleton s2 = new Singleton(Stream.empty());
+    Set<?> singleton = s2.getSingleton();
+    assertEquals("[]", singleton.toString());
+
+    Singleton s3 = new Singleton(Stream.of(1));
+    singleton = s3.getSingleton();
+    assertEquals("[1]", singleton.toString());
+  }
+
 }
