480a307f9f6f0316a9724bb76e750d9240d4e728
Hiram Kamau <hiram.kamau@outlook.com>
2021-11-15 13:19:34 +0300

    Update ShuffleTest.java

--- collectingAndThen/src/test/java/com/github/idelstak/collectingandthen/ShuffleTest.java
+++ collectingAndThen/src/test/java/com/github/idelstak/collectingandthen/ShuffleTest.java
@@ -14,16 +14,16 @@ import static org.junit.Assert.assertNotEquals;
  * @author Hiram K. <https://github.com/IdelsTak>
  */
 public class ShuffleTest {
-    
-    public ShuffleTest() {
-    }
 
-    @Test
-    public void shouldShuffleList() {
-        Shuffle shuffle = new Shuffle(Stream.of(1,2,3));
-        List<?> list = shuffle.getList();
-        
-        assertNotEquals("[1, 2, 3]", list.toString());
-    }
-    
+  public ShuffleTest() {
+  }
+
+  @Test
+  public void shouldShuffleList() {
+    Shuffle shuffle = new Shuffle(Stream.of(1, 2, 3));
+    List<?> list = shuffle.getList();
+
+    assertNotEquals("[1, 2, 3]", list.toString());
+  }
+
 }
