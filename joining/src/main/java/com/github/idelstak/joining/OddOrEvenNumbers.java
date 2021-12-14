16b8eb54ebb04292f9d26e79784b0d9c88674d5f
Hiram Kamau <hiram.kamau@outlook.com>
2021-12-14 12:28:34 +0300

    Update OddOrEvenNumbers.java

--- joining/src/main/java/com/github/idelstak/joining/OddOrEvenNumbers.java
+++ joining/src/main/java/com/github/idelstak/joining/OddOrEvenNumbers.java
@@ -3,10 +3,11 @@
  */
 package com.github.idelstak.joining;
 
-import java.util.Arrays;
 import java.util.function.IntPredicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.Arrays.stream;
 
 /**
  *
@@ -19,22 +20,23 @@ public class OddOrEvenNumbers {
 
   public OddOrEvenNumbers(IntPredicate predicate, int[] numbers) {
     this.predicate = predicate;
-    this.numbers = Arrays.stream(numbers);
+    this.numbers = stream(numbers);
   }
 
   public static void main(String[] args) {
     IntPredicate oddPredicate = i -> i % 2 != 0;
     int[] arr = new int[]{1, 2, 3, 4};
-    System.out.println("Odd numbers: " + new OddOrEvenNumbers(oddPredicate, arr).getValue());
+    System.out.println(new OddOrEvenNumbers(oddPredicate, arr));
 
     IntPredicate evenPredicate = i -> i % 2 == 0;
-    System.out.println("Even numbers: " + new OddOrEvenNumbers(evenPredicate, arr).getValue());
+    System.out.println(new OddOrEvenNumbers(evenPredicate, arr));
 
     arr = new int[]{1, 3, 5, 7};
-    System.out.println("Even numbers: " + new OddOrEvenNumbers(evenPredicate, arr).getValue());
+    System.out.println(new OddOrEvenNumbers(evenPredicate, arr));
   }
 
-  public String getValue() {
+  @Override
+  public String toString() {
     return numbers
             .filter(predicate)
             .mapToObj(Integer::toString)
