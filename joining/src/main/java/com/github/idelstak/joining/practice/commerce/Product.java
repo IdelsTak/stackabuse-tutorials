99b5f0a45c3c01fb7cf55be2722e19bdbb1acfc2
Hiram Kamau <hiram.kamau@outlook.com>
2021-12-15 10:01:19 +0300

    Update Product.java

--- joining/src/main/java/com/github/idelstak/joining/practice/commerce/Product.java
+++ joining/src/main/java/com/github/idelstak/joining/practice/commerce/Product.java
@@ -4,11 +4,9 @@
 package com.github.idelstak.joining.practice.commerce;
 
 import java.util.Objects;
-import java.util.StringJoiner;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
-import static java.lang.System.lineSeparator;
 import static java.util.stream.Collectors.joining;
 
 /**
@@ -21,43 +19,75 @@ public class Product {
   private final String material;
   private final String name;
 
+  private static final Predicate<String> NON_NULL = Objects::nonNull;
+  private static final Predicate<String> EMPTY = String::isEmpty;
+  private static final Predicate<String> NON_EMPTY = EMPTY.negate();
+  private static final Predicate<String> EMPTY_TRIMMED = s -> s.trim().isEmpty();
+  private static final Predicate<String> NON_EMPTY_TRIMMED = EMPTY_TRIMMED.negate();
+
   public Product(String attribute, String material, String name) {
     this.attribute = attribute;
     this.material = material;
     this.name = name;
   }
 
-  @Override
-  public String toString() {
-    Predicate<String> nonNull = Objects::nonNull;
-    Predicate<String> empty = String::isEmpty;
-    Predicate<String> nonEmpty = empty.negate();
-    Predicate<String> emptyTrimmed = s -> s.trim().isEmpty();
-    Predicate<String> nonEmptyTrimmed = emptyTrimmed.negate();
-
-    return Stream.of(attribute, material, name)
-            .filter(nonNull.and(nonEmpty).and(nonEmptyTrimmed))
-            .collect(joining(" "));
+  public String getAttribute() {
+    return attribute;
   }
 
-  public static void main(String[] args) {
+  public String getMaterial() {
+    return material;
+  }
 
-    String descriptions = Stream
-            .of(
-                    new Product("Heavy Duty", "Rubber", "Plate"),
-                    new Product("Ergonomic", "Rubber", "Computer"),
-                    new Product("Durable", "Cotton", "Bench"),
-                    new Product("Intelligent", "Silk", "Lamp"),
-                    new Product("Enormous", "Granite", "Shoes"),
-                    new Product("Practical", "", "Watch"),
-                    new Product("Gorgeous", null, "Bench"),
-                    new Product("Ergonomic", "Paper", "Pants"),
-                    new Product("Enormous", "Copper", "Bag"),
-                    new Product("Fantastic", " ", "Keyboard")
-            )
-            .map(Product::toString)
-            .collect(joining(lineSeparator()));
+  public String getName() {
+    return name;
+  }
+
+//  @Override
+//  public String toString() {
+//    return Stream.of(this.getAttribute(), this.getMaterial(), this.getName())
+//            .filter(NON_NULL.and(NON_EMPTY).and(NON_EMPTY_TRIMMED))
+//            .collect(joining(" "));
+//  }
+  @Override
+  public String toString() {
+    return getDescription(this);
+  }
+//  @Override
+//  public String toString() {
+//    Stream<String> features = Stream.of(attribute, material, name);
+//    return features.collect(joining(" "));
+//  }
+
+  public static void main(String[] args) {
+    Stream<Product> products = Stream.of(
+            new Product("Heavy Duty", "Rubber", "Plate"),
+            new Product(" ", "Rubber", "Computer"),
+            new Product("Durable", "Cotton", "Bench"),
+            new Product("Intelligent", "Silk", "Lamp"),
+            new Product("Enormous", "Granite", "Shoes"),
+            new Product("Practical", "", "Watch"),
+            new Product("Gorgeous", null, "Bench"),
+            new Product("Ergonomic", "Paper", "Pants"),
+            new Product("", "Copper", "Bag"),
+            new Product("Fantastic", " ", "Keyboard")
+    );
+
+//    String descriptions = products
+//            .map(Object::toString)
+//            .collect(joining(System.lineSeparator()));
+//
+//    System.out.println(descriptions);
+    String descriptions = products
+            .map(Product::getDescription)
+            .collect(joining(System.lineSeparator()));
 
     System.out.println(descriptions);
+  }
+
+  public static String getDescription(Product product) {
+    return Stream.of(product.getAttribute(), product.getMaterial(), product.getName())
+            .filter(NON_NULL.and(NON_EMPTY).and(NON_EMPTY_TRIMMED))
+            .collect(joining(" "));
   }
 }
