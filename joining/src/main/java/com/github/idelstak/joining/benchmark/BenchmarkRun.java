0f6ec0f6ca5e4dc4e27920ab764178e3c51c34fd
Hiram Kamau <hiram.kamau@outlook.com>
2021-12-16 15:42:22 +0300

    Create BenchmarkRun.java

---
+++ joining/src/main/java/com/github/idelstak/joining/benchmark/BenchmarkRun.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2021
+ */
+package com.github.idelstak.joining.benchmark;
+
+import java.io.IOException;
+import org.openjdk.jmh.Main;
+
+/**
+ *
+ * @author Hiram K. <https://github.com/IdelsTak>
+ */
+public class BenchmarkRun {
+
+  public static void main(String[] args) throws IOException {
+    Main.main(args);
+  }
+}
