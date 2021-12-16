1ede528220776d954cf633a3e62a6c0c5711b301
Hiram Kamau <hiram.kamau@outlook.com>
2021-12-16 13:44:12 +0300

    Update OxfordComma.java

--- joining/src/main/java/com/github/idelstak/joining/practice/grammar/OxfordComma.java
+++ joining/src/main/java/com/github/idelstak/joining/practice/grammar/OxfordComma.java
@@ -4,6 +4,9 @@
 package com.github.idelstak.joining.practice.grammar;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.collectingAndThen;
@@ -23,32 +26,32 @@ public class OxfordComma {
   }
 
   public String generate() {
-    return Arrays.stream(words)
-            .collect(
-                    collectingAndThen(
-                            toList(),
-                            l -> {
-                              String value = "[No words were provided]";
+    Collector<String, ?, List<String>> downstream = toList();
+    Function<List<String>, String> finisher = l -> {
+      String value = "[No words were provided]";
 
-                              if (l.size() == 1) {
-                                value = l.stream().collect(joining(""));
-                              } else if (l.size() == 2) {
-                                value = l.stream().collect(joining(" and "));
-                              } else if (l.size() > 2) {
-                                int lastIdx = l.size() - 1;
-                                String first = l.stream().limit(lastIdx).collect(joining(", "));
-                                String last = l.get(lastIdx);
-                                value = Stream.of(first, last).collect(joining(", and "));
-                              }
+      if (l.size() == 1) {
+        value = l.stream().collect(joining(""));
+      } else if (l.size() == 2) {
+        value = l.stream().collect(joining(" and "));
+      } else if (l.size() > 2) {
+        int lastIdx = l.size() - 1;
+        String first = l.stream().limit(lastIdx).collect(joining(", "));
+        String last = l.get(lastIdx);
+        value = Stream.of(first, last).collect(joining(", and "));
+      }
 
-                              return value;
-                            }
-                    ));
+      return value;
+    };
+
+    return Arrays
+            .stream(words)
+            .collect(collectingAndThen(downstream, finisher));
   }
 
   public static void main(String[] args) {
     OxfordComma comma = new OxfordComma("maroon", "lavender", "olive", "blue");
-    
+
     System.out.println(comma.generate());
   }
 }
